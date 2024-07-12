5e4e207265c80ced9022984b18bface0945ab050
贺俊浩 <nougat_hx@qq.com>
2024-07-12 15:59:32 +0800

    fix for

--- src/main.rs
+++ src/main.rs
@@ -46,8 +46,8 @@ fn handle_client(mut stream: TcpStream) {
                             continue;
                         }
                     }
-                    send_notification(&title, &msg).unwrap();
                 }
+                send_notification(&title, &msg).unwrap();
                 let response = "HTTP/1.1 200 OK\r\n\r\n";
                 stream.write_all(response.as_bytes()).unwrap();
             }
